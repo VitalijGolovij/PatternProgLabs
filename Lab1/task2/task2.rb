6978ae7d2f057955aca9e5049a86b1fcf0b81f2a
vitalijgolovij <vitalijgl19@gmail.com>
2023-02-05 21:46:43 +0300

    add lab1 task2 method3

--- Lab1/task2/task2.rb
+++ Lab1/task2/task2.rb
@@ -1,6 +1,21 @@
-def coprime?(number1, number2)
+def coprime?(number1, number2) 
   2.upto([number1, number2].min).each {|i| return false if (number1 % i).zero? and (number2 % i).zero?}
   true
+end
+
+def get_small_div(number)
+  2.upto(number).each {|i|return i if (number%i).zero?}
+end
+
+def get_max_coprime(number)
+  number.downto(2).each {|val| return val if not coprime?(val, number) and not (val % get_small_div(number)).zero?}
+  return -1
+end
+
+def get_sum_dig_less5(number)
+  digit_sum = 0
+  number.digits.each {|dig| digit_sum += dig if dig<5}
+  digit_sum
 end
 
 def count_even_notcoprime(number)
@@ -15,7 +30,16 @@ def max_dig_notdiv3(number)
   answ
 end
 
+def mult_summ_dig(number)
+  puts "Im mult #{get_max_coprime(number)} and #{get_sum_dig_less5(number)}"
+  get_max_coprime(number) * get_sum_dig_less5(number)
+end
+
+
 puts 'Введите число:'
 number = STDIN.gets.to_i
 puts "Количество четных и невзаимно простых с данным: #{count_even_notcoprime(number)}"
 puts "Максимальная цифра числа, не делящаяся на 3: #{max_dig_notdiv3(number)}"
+puts "произведение максимального числа, не взаимно простого
+с данным, не делящегося на наименьший делитель исходно числа, и
+суммы цифр числа, меньших 5: #{mult_summ_dig(number)}"
