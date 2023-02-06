dae3cfc259a4983e2507d66fc77757fb884ab02f
vitalijgolovij <vitalijgl19@gmail.com>
2023-02-06 21:09:13 +0300

    add lab1 task3 method3

--- Lab1/task4/task4.rb
+++ Lab1/task4/task4.rb
@@ -1,8 +1,14 @@
 def idx_desc_order(array)
   return nil if array.nil?
-  puts array.map.with_index { |x, i| [x,i] }.sort_by { |x| x[0] }.reverse.map{|x| x[1]}
+  array.map.with_index { |val, idx| [val, idx] }.sort_by { |val| val[0] }.reverse.map{|val| val[1]}
+end
+
+def val_btw_first_two_max(array)
+  max_idx1 = array.find_index(array.max)
+  tmp_array = array.reject.with_index{|val, idx| idx==max_idx1}
+  max_idx2 = tmp_array.find_index(tmp_array.max)
+  if max_idx1<=max_idx2 then max_idx2 += 1 end
+  array[max_idx1+1...max_idx2]
 end
 
 
-
-puts idx_desc_order(a)
