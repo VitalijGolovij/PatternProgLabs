deead7690f6ddb3caadf6d3309250cc81b311ebc
vitalijgolovij <vitalijgl19@gmail.com>
2023-03-09 23:32:28 +0300

    протестировал метод чтения с файла

--- Lab2/task1/main.rb
+++ Lab2/task1/main.rb
@@ -2,18 +2,6 @@ require 'json'
 require File.expand_path(File.dirname(__FILE__) + '/Student.rb')
 require File.expand_path(File.dirname(__FILE__) + '/Student_short.rb')
 
-student4 = Student.new(id:1, name:'Егор',surname:'Крид',patronymic: 'Валерьевич', git:'github.com/aaa', mail:'aa@maiil.com')
-student4.set_contacts(phone:'+70000000000')
+l = Student.read_from_txt('input.txt')
 
-
-short1 = Student_short.new(3,Student_short.to_json_str("Буквоедов А. Н.","github.com/sss"))
-puts short1.id
-puts short1.shortname
-puts short1.contact
-puts short1.git
-
-short2 = Student_short.from_student(student4)
-puts short2.id
-puts short2.shortname
-puts short2.contact
-puts short2.git
+puts l
