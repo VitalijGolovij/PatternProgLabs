2b267af9a56c1c31aea9ad15e05be4098e309112
vitalijgolovij <vitalijgl19@gmail.com>
2023-03-17 16:11:50 +0300

    добавил методы получения элемента, кол-во строк\стобцов

--- Lab2/task1/data_table.rb
+++ Lab2/task1/data_table.rb
@@ -10,4 +10,18 @@ class Data_table
       @table << row
     end
   end
+
+  def get_elem(index_row, index_column)
+    @table[index_row][index_column]
+  end
+
+  def rows_count
+    @table.size
+  end
+
+  def columns_count
+    count = 0
+    @table.each { |row| count = row.size if row.size > count }
+    count
+  end
 end
