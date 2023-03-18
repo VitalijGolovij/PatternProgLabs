e9a68008ca57abd8b3df998cb275c1dbdbec3003
vitalijgolovij <vitalijgl19@gmail.com>
2023-03-18 16:26:03 +0300

    переписал снова data_table

--- Lab2/task1/data_table.rb
+++ Lab2/task1/data_table.rb
@@ -1,15 +1,9 @@
-# frozen_string_literal: true
 require 'json'
 require_relative 'data_list'
 
 class Data_table
-  def initialize(objects_array)
-    @table = []
-    objects_array.each do |object|
-      row = []
-      object.instance_variables.each{|attr| row << object.instance_variable_get(attr)}
-      @table << row
-    end
+  def initialize(objects_table)
+    @table = objects_table
   end
 
   def get_elem(index_row, index_column)
