71cc7574bb4e95753deef1be5327507baa1f06ab
vitalijgolovij <vitalijgl19@gmail.com>
2023-03-27 18:58:22 +0300

    сделал student_list_yaml

---
+++ Lab2/task1/student_list_yaml.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+require 'yaml'
+
+class Student_list_YAML
+  attr_reader :student_list
+
+  def initialize(file_path)
+    @student_list = []
+    @id_counter = 1
+    YAML.load(File.read(file_path)).each{|hash_for_student| put_student(Student.new(hash_for_student))}
+  end
+
+  def write_to_yaml(file_path)
+    student_hash_list = []
+    @student_list.each{ |student| student_hash_list.append(student.to_hash) }
+    File.write(file_path, YAML.dump(student_hash_list))
+  end
+
+  def get_student_by_id(id)
+    @student_list.find{ |student| student.id == id }
+  end
+
+  def get_k_n_student_short_list(k, n, data_list = nil)
+
+    if data_list
+      raise ArgumentError, "overflow indexes" if data_list.list.size < k * (n - 1) + 1
+      Data_list.new(data_list.list[k * (n - 1), k])
+    else
+      raise ArgumentError, "overflow indexes" if @student_list.size < k * (n - 1) + 1
+      Data_list.new(@student_list[k * (n - 1), k].map { |student| Student_short.from_student(student) } )
+    end
+  end
+
+  def sort_by_shortname
+    @student_list.sort_by! { |student | student.shortname }
+  end
+
+  def put_student(student, index = @student_list.size)
+    raise ArgumentError, "arg must be Student" unless student.class == Student
+
+    student.id = @id_counter
+    @student_list[index] = student
+    @id_counter += 1
+  end
+
+  def replace_by_id(id, student)
+    replace_index = @student_list.find_index{|old_student| old_student.id == id}
+    put_student(student, replace_index)
+  end
+
+  def drop_by_id(id)
+    @student_list.delete_if {|student| student.id == id}
+  end
+
+  def get_student_short_count
+    @student_list.size
+  end
+end
