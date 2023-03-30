a43e937e19a3924f86df62aa79779d1774d0158c
vitalijgolovij <vitalijgl19@gmail.com>
2023-03-30 19:14:00 +0300

    выделил суперкласс student_list

---
+++ Lab2/task1/student_list.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+require_relative 'data_list'
+
+class Student_list < Data_list
+  attr_reader :student_list
+
+  private_class_method :new
+
+  def initialize
+    self.list=[]
+    @id_counter = 1
+  end
+
+  def get_student_by_id(id)
+    self.list.find{ |student| student.id == id }
+  end
+
+  def get_k_n_student_short_list(k, n, data_list = nil)
+    if data_list
+      raise ArgumentError, "overflow indexes" if data_list.list.size < k * (n - 1) + 1
+      Data_list_student_short.new(data_list.list[k * (n - 1), k])
+    else
+      raise ArgumentError, "overflow indexes" if self.list.size < k * (n - 1) + 1
+      Data_list_student_short.new(self.list[k * (n - 1), k].map { |student| Student_short.from_student(student) } )
+    end
+  end
+
+  def sort_by_shortname
+    self.list.sort_by! { |student | student.shortname }
+  end
+
+  def put_student(student, index = self.list.size)
+    raise ArgumentError, "arg must be Student" unless student.class == Student
+
+    student.id = @id_counter
+    self.list[index] = student
+    @id_counter += 1
+  end
+
+  def replace_by_id(id, student)
+    replace_index = self.list.find_index{|old_student| old_student.id == id}
+    put_student(student, replace_index)
+  end
+
+  def drop_by_id(id)
+    self.list.delete_if {|student| student.id == id}
+  end
+
+  def get_student_short_count
+    self.list.size
+  end
+end
--- Lab2/task1/student_list_txt.rb
+++ Lab2/task1/student_list_txt.rb
@@ -1,53 +1,15 @@
 # frozen_string_literal: true
+require_relative 'student_list'
 
-class Student_list_txt
+class Student_list_txt < Student_list
+  public_class_method :new
+
   def initialize(txt_file_path)
-    @student_list = []
-    @id_counter = 1
+    super()
     Student.read_from_txt(txt_file_path).each { |student| put_student(student) }
   end
 
   def write_to_txt(file_path)
-    Student.write_to_txt(@student_list, file_path)
-  end
-
-  def get_student_by_id(id)
-    @student_list.find{ |student| student.id == id }
-  end
-
-  def get_k_n_student_short_list(k, n, data_list = nil)
-
-    if data_list
-      raise ArgumentError, "overflow indexes" if data_list.list.size < k * (n - 1) + 1
-      Data_list.new(data_list.list[k * (n - 1), k])
-    else
-      raise ArgumentError, "overflow indexes" if @student_list.size < k * (n - 1) + 1
-      Data_list.new(@student_list[k * (n - 1), k].map { |student| Student_short.from_student(student) } )
-    end
-  end
-
-  def sort_by_shortname
-    @student_list.sort_by! { |student | student.shortname }
-  end
-
-  def put_student(student, index = @student_list.size)
-    raise ArgumentError, "arg must be Student" unless student.class == Student
-
-    student.id = @id_counter
-    @student_list[index] = student
-    @id_counter += 1
-  end
-
-  def replace_by_id(id, student)
-    replace_index = @student_list.find_index{|old_student| old_student.id == id}
-    put_student(student, replace_index)
-  end
-
-  def drop_by_id(id)
-    @student_list.delete_if {|student| student.id == id}
-  end
-
-  def get_student_short_count
-    @student_list.size
+    Student.write_to_txt(self.list, file_path)
   end
 end
