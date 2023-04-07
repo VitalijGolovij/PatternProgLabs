2f2055c13d0abd237fc624a2034ca5285439be47
vitalijgolovij <vitalijgl19@gmail.com>
2023-04-07 23:10:33 +0300

    подправил student_list_db

--- Lab2/Student/Lists_models/student_list_db.rb
+++ Lab2/Student/Lists_models/student_list_db.rb
@@ -4,48 +4,39 @@ require_relative '../database/class/database_student_worker'
 require 'mysql2'
 require 'yaml'
 
-class Student_list_DB < Student_list
+class Student_list_DB
   def initialize(options = {})
     @db_worker = Database_student_worker.new(options)
-    update_list
-  end
-
-  protected def update_list
-    self.list = @db_worker.select_all
   end
 
   def get_student_by_id(id)
-    update_list
-    super
+    Student.new(@db_worker.select_by_id('students', id))
   end
 
   def get_k_n_student_short_list(k, n, data_list = nil)
-    update_list
-    super
+    if data_list
+      raise ArgumentError, "overflow indexes" if data_list.list.size < k * (n - 1) + 1
+      Data_list_student_short.new(data_list.list[k * (n - 1), k])
+    else
+      students_list = @db_worker.select('students').map{|student_hash| Student.new(student_hash)}
+      raise ArgumentError, "overflow indexes" if students_list.size < k * (n - 1) + 1
+      Data_list_student_short.new(students_list[k * (n - 1), k].map { |student| Student_short.from_student(student) } )
+    end
   end
 
-  def put_student(student)
-    @db_worker.insert_student(student.to_hash)
-    update_list
+  def insert_student(student)
+    @db_worker.insert('students', student.to_hash)
   end
 
   def replace_by_id(id, student)
-    @db_worker.update_by_id(id, student.to_hash)
-    update_list
+    @db_worker.update_by_id('students', id, student.to_hash)
   end
 
   def drop_by_id(id)
-    @db_worker.delete_by_id(id)
-    update_list
+    @db_worker.delete_by_id('students', id)
   end
 
-  def get_student_short_count
-    update_list
-    super
-  end
-
-  def list
-    update_list
-    @list
+  def get_students_count
+    @db_worker.select('students').size
   end
 end
