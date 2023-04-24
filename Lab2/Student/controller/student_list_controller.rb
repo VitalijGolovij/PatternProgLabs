6f4720897164e56dcb4993205f6c7fd0a28965ba
vitalijgolovij <vitalijgl19@gmail.com>
2023-04-24 20:46:24 +0300

    обработка ошибки подключения к бд

--- Lab2/Student/controller/student_list_controller.rb
+++ Lab2/Student/controller/student_list_controller.rb
@@ -2,11 +2,18 @@
 require_relative '../Lists_models/student_list'
 require_relative '../Lists_models/student_list_file_adapter'
 require_relative '../Lists_models/student_list_json'
+require_relative '../Lists_models/student_list_db'
+require 'yaml'
+require 'json'
+require 'fox16'
+include Fox
 
 class Student_list_controller
   def initialize(view)
     @view = view
-    @student_list = Student_list.new(Student_list_file_adapter.new(Student_list_JSON.new, 'result_files/input.json'))
+
+    # @student_list = Student_list.new(Student_list_file_adapter.new(Student_list_JSON.new, 'result_files/input.json'))
+    @student_list = Student_list.new(Student_list_DB.new(YAML.load(File.open('database/config/connect_db_hash.yaml'))))
     @cur_table_page = 1
     @all_table_pages = (@student_list.get_students_count.to_f / 20.0).ceil
   end
