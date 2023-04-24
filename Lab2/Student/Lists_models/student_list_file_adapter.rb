24d4c8bb4c66cd6731b0f0937647f6828cc65e10
vitalijgolovij <vitalijgl19@gmail.com>
2023-04-24 23:49:29 +0300

    чуть подредактировал адаптер

--- Lab2/Student/Lists_models/student_list_file_adapter.rb
+++ Lab2/Student/Lists_models/student_list_file_adapter.rb
@@ -12,29 +12,35 @@ class Student_list_file_adapter < Student_list_data_worker
   end
 
   def get_student_by_id(id)
+    @student_list_file.read_from_file(@file_path)
     @student_list_file.get_student_by_id(id)
   end
 
   def get_k_n_student_short_list(k, n, data_list = nil)
+    @student_list_file.read_from_file(@file_path)
     @student_list_file.get_k_n_student_short_list(k,n,data_list)
   end
 
   def replace_by_id(id, student)
+    @student_list_file.read_from_file(@file_path)
     @student_list_file.replace_by_id(id, student)
     @student_list_file.write_to_file(@file_path)
   end
 
   def insert_student(student)
+    @student_list_file.read_from_file(@file_path)
     @student_list_file.insert_student(student)
     @student_list_file.write_to_file(@file_path)
   end
 
   def drop_by_id(id)
+    @student_list_file.read_from_file(@file_path)
     @student_list_file.drop_by_id(id)
     @student_list_file.write_to_file(@file_path)
   end
 
   def get_students_count
+    @student_list_file.read_from_file(@file_path)
     @student_list_file.get_student_count
   end
 end
